d12092f1dfdb3dbaf24fc46aca7c6b37b6c7a45b
iskandarasri03 <71484902+iskandarasri03@users.noreply.github.com>
2022-05-14 21:43:33 +0800

    Rebase spes to fog

--- nad_spes.mk
+++ nad_spes.mk
@@ -9,25 +9,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from spes/spesn device
-$(call inherit-product, device/xiaomi/spes/device.mk)
+$(call inherit-product, device/xiaomi/fog/device.mk)
 
-# Inherit some common Nusantara stuff.
-$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
-NAD_BUILD_TYPE := OFFICIAL
-TARGET_USES_BLUR := true
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_FACE_UNLOCK_SUPPORTED := true
-USE_PIXEL_CHARGING := true
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Product Specifics
-PRODUCT_NAME := nad_spes
-PRODUCT_DEVICE := spes
+PRODUCT_NAME := lineage_fog
+PRODUCT_DEVICE := fog
 PRODUCT_BRAND := Redmi
-PRODUCT_MODEL := Redmi Note 11
+PRODUCT_MODEL := Redmi 10C
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-# GMS
-TARGET_GAPPS_ARCH := arm64
-USE_GAPPS := true
